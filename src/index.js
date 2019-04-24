b26bb972889a70f1dd0abcade7259ef974f5f8d4
sepuckett86 <sepuckett86@gmail.com>
2019-04-23 17:04:57 -0700

    add button event handler

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ const input2 = document.getElementById('Question2');
 const input3 = document.getElementById('Question3'); 
 const input4 = document.getElementById('Question4');
 const input5 = document.getElementById('Question5');
+const button = document.getElementById('submit-button');
 const resultP = document.getElementById('Results');
+
+button.addEventListener('click', (event) => {
+    console.log('click!');
+});
