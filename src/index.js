b8815be41d7b0d02fd735d370ca7699c51886254
sepuckett86 <sepuckett86@gmail.com>
2019-04-23 17:00:14 -0700

    define variables in index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const input1 = document.getElementById('Question1');
+const input2 = document.getElementById('Question2');
+const input3 = document.getElementById('Question3'); 
+const input4 = document.getElementById('Question4');
+const input5 = document.getElementById('Question5');
