600d1a1c42d1161503e08bc330874a18bc948f4b
sepuckett86 <sepuckett86@gmail.com>
2019-04-23 17:16:51 -0700

    modify function to pass test

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,23 @@
 const test = QUnit.test;
 
 function scoreAnswers(answer1, answer2, answer3, answer4, answer5) {
-    return 5;
+    let total = 0;
+    if(answer1 === 'sloth') {
+        total += 1;
+    }
+    if(answer2 === 'sloth') {
+        total += 1;
+    }
+    if(answer3 === 'bamboo') {
+        total += 1;
+    }
+    if(answer4 === 'manatee') {
+        total += 1;
+    }
+    if(answer5 === 3) {
+        total += 1;
+    }
+    return total;
 }
 
 test('return 5 if all answers are correct', (assert) => {
@@ -13,7 +29,7 @@ test('return 5 if all answers are correct', (assert) => {
     const answer5 = 3;
     const expected = 5;
     // Act
-    const actual = scoreAnswers();
+    const actual = scoreAnswers(answer1, answer2, answer3, answer4, answer5);
     // Assert
     assert.equal(actual, expected);
 });
@@ -27,7 +43,7 @@ test('return 0 if all answers are incorrect', (assert) => {
     const answer5 = 1;
     const expected = 0;
     // Act
-    const actual = scoreAnswers();
+    const actual = scoreAnswers(answer1, answer2, answer3, answer4, answer5);
     // Assert
     assert.equal(actual, expected);
 });
